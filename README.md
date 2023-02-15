790886dd5abff7e3662820990b2e8bf45f1220a2
ankitd <dhimanankit262@gmail.com>
2023-02-15 18:25:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Fire Ranger
+#### Fire ranger is a deep learning based model to predict wild fires from satellite images.
+
+The web app has been made using streamlit
+
+Requirements
+============
+First install the requirements using `pip install -r requirements.txt`
+
+Usage
+=====
+`
+`
---
+++ myapp.py
@@ -0,0 +1,33 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+
+import io
+import numpy as np
+import tensorflow as tf
+import streamlit as st
+from PIL import Image
+
+st.write('''
+	# Fire Ranger
+	#### Deep Learning model to predict wildfires from satellite images
+''')
+
+model = tf.keras.models.load_model('./models/final_model.h5')
+
+img = Image.open('./images/test.jpg')
+img = np.array(img).reshape((1,350,350,3))
+
+uploaded_img = st.file_uploader(
+	label = 'Upload a 350x350 satellite image',
+	type = ['png','jpg','jpeg'])
+
+if uploaded_img is not None:
+	file = uploaded_img.read()
+	img = Image.open(io.BytesIO(file))
+	img = np.array(img).reshape((1,350,350,3))
+
+pred = model.predict(img)
+
+st.image(image = img)
+st.text(f'Wildfire prediction probability is {pred[0,1]*100}%')
+
